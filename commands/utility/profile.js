835e603551ee0f1943de04d38a47c7e8c70ccb73
Huu <63862081+8BitBlimp@users.noreply.github.com>
2022-10-12 12:16:12 +0200

    Update profile.js

--- commands/utility/profile.js
+++ commands/utility/profile.js
@@ -7,19 +7,34 @@ const User = require('../../modules/user.js')
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('profile')
-		.setDescription('Check your profile'),
+		.setDescription('Check your profile')
+		.addUserOption(option => option.setName('user').setDescription('Select the user you want to see the profile of.').setRequired(false)),
 	async execute(interaction) {
         // enter command here
 
-
+		let something = interaction.options.getUser('user')
 		let userData = await User.findOne({ user: interaction.user.id })
-		let profileEmbed = new EmbedBuilder()
-			.setTitle(`${interaction.user.username}'s profile stats`)
-			.setThumbnail(interaction.user.displayAvatarURL({ dynamic: true }))
-			.setDescription(`Social Credit: ${userData.rep}
-		Level: ${userData.level}
-		XP: ${userData.xp}`)
-		await interaction.reply({ embeds: [profileEmbed] })
+
+		if(something) {
+			userData = await User.findOne({ user: something.id })
+			let profileEmbed = new EmbedBuilder()
+				.setTitle(`${something.username}'s profile stats`)
+				.setThumbnail(something.displayAvatarURL({ dynamic: true }))
+				.setDescription(`Social Credit: ${userData.rep}
+			Level: ${userData.level}
+			XP: ${userData.xp}`)
+			await interaction.reply({ embeds: [profileEmbed] })
+		} else {
+			let profileEmbed = new EmbedBuilder()
+				.setTitle(`${interaction.user.username}'s profile stats`)
+				.setThumbnail(interaction.user.displayAvatarURL({ dynamic: true }))
+				.setDescription(`Social Credit: ${userData.rep}
+			Level: ${userData.level}
+			XP: ${userData.xp}`)
+			await interaction.reply({ embeds: [profileEmbed] })
+		}
+
+		
 
 
 	},
